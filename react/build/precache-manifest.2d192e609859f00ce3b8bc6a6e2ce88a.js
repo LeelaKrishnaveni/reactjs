f514aa68cb40dc08aefee9f2f24b89e388c1cecc
LeelaKrishnaveni <61830666+LeelaKrishnaveni@users.noreply.github.com>
2021-02-24 16:09:51 +0530

    Add files via upload

---
+++ react/build/precache-manifest.2d192e609859f00ce3b8bc6a6e2ce88a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e721a7ab6afdb16ec10db9551dc19b1b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7431b2149ac0013bdcfa",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "fb43a7123a96736755ac",
+    "url": "/static/js/2.8bb93952.chunk.js"
+  },
+  {
+    "revision": "7431b2149ac0013bdcfa",
+    "url": "/static/js/main.66e80230.chunk.js"
+  },
+  {
+    "revision": "0773f84eabf7bdfde857",
+    "url": "/static/js/runtime~main.963d1b26.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
